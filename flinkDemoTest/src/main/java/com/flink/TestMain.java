d6aba6dea9c8d048fade48f93165bf31b332f0c2
1105748319 <1105748319@qq.com>
2020-12-07 09:54:51 +0800

    上传

--- flinkDemoTest/src/main/java/com/flink/TestMain.java
+++ flinkDemoTest/src/main/java/com/flink/TestMain.java
@@ -20,7 +20,6 @@ public class TestMain {
 	protected static StreamExecutionEnvironment env;
 	protected static StreamTableEnvironment tEnv;
 	protected static int ff = 0;
-
 	public static void main(String[] args) throws Exception {
 		try {
 			env = StreamExecutionEnvironment.getExecutionEnvironment();
